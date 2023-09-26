4e9c3db4b3251cc012798943516608f1f744fbf5
root <root@ip-172-31-88-98.ec2.internal>
2023-09-26 13:59:22 +0000

    added comments

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-backend-sept-prasad"
+    bucket         = "terraform-backend-sept-prasad" #Change the name 
     encrypt        = true
     key            = "terraform.tfstate"
     region         = "us-east-1"
